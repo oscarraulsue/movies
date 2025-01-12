3dbaadfcf7fb1696ae7fc5ce5e120765959ac653
Oscar Sue <oscarsue001@gmail.com>
2025-01-12 10:56:51 -0500

    Enhance NotFound component with improved layout and messaging

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,34 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <>
+      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold text-indigo-600">404</p>
+          <h1 className="mt-4 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
+            Pagina no encontrada
+          </h1>
+          <p className="mt-6 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+            La página que buscas no existe o ha sido movida.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Link
+              href="/"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Ir al inicio
+            </Link>
+            <a
+              href="mailto:oscarsue001@gmail.com"
+              rel="noreferrer"
+              target="_blank"
+              className="text-sm font-semibold text-gray-900"
+            >
+              Contactar a soporte <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
